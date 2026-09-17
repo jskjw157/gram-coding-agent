a69b19dc2c7afcb3fe02ef430e8cd2eb116de520
johny <69754601+jskjw157@users.noreply.github.com>
2026-09-17 19:14:18 +0900

    feat: export repository persistence adapter

--- packages/persistence/src/index.ts
+++ packages/persistence/src/index.ts
@@ -1,4 +1,5 @@
 export * from './database.js';
 export * from './migrator.js';
 export * from './repositories/audit-repository.js';
+export * from './repositories/repository-repository.js';
 export * from './repositories/task-repository.js';
